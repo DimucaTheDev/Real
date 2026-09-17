c440d9c15fcd615c3a4d74955518d03fea97e0ec
DimucaTheDev <dima.punchenko@gmail.com>
2026-09-17 21:59:32 +0300

    123

--- Real/SoundTest.cs
+++ Real/SoundTest.cs
@@ -1,11 +1,41 @@
 using FMOD;
+using Thread = System.Threading.Thread;
 
 namespace Real;
 
 public class SoundTest
 {
-    public static string Test()
+    public static void Test()
     {
-        return FMOD.Error.String(FMOD.Studio.System.create(out _));
+        Check(Factory.System_Create(out FMOD.System system));
+        Check(system.init(32, INITFLAGS.NORMAL, IntPtr.Zero));
+
+        Check(system.createSound("yay.mp3", MODE.DEFAULT | MODE.CREATESTREAM | MODE.LOOP_OFF, out Sound sound));
+
+        Check(system.playSound(sound, new ChannelGroup(), false, out _));
+
+        bool isPlaying = true;
+        while (isPlaying)
+        {
+            Check(system.update());
+
+            Check(system.getChannelsPlaying(out int channelsPlaying));
+            if (channelsPlaying == 0)
+            {
+                isPlaying = false;
+            }
+
+            Thread.Sleep(10);
+        }
+
+        Check(sound.release());
+        Check(system.close());
+        Check(system.release());
+    }
+
+    private static void Check(RESULT result)
+    {
+        if (result != RESULT.OK)
+            throw new InvalidOperationException($"Ошибка FMOD: {result}");
     }
 }
